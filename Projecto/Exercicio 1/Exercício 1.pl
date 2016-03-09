eefb449ac0f3c35cef19c7d1a09fe134d6143eb2
tigas95 <tiago.carvalhais.1995@gmail.com>
2016-03-09 10:14:50 +0000

    comecei a fazer

---
+++ Projecto/Exercicio 1/Exercício 1.pl
@@ -0,0 +1,14 @@
+% utente: Nome, Serviço, Profissional, Instituição -> {V, F}
+% servico: Designação, Instituição -> {V, F}
+% profissional: Nome, Serviço, Instituição -> {V, F}
+
+
+% Base de Conhecimento sobre Utentes -------------------------------------------------------------------------------------------
+
+
+
+% Base de Conhecimento sobre Serviços ------------------------------------------------------------------------------------------
+
+
+
+% Base de Conhecimento sobre Profissionais -------------------------------------------------------------------------------------
