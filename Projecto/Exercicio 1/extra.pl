c81917a38a14d0cbd88478470cdad2a049bd2a8e
zecarlos94 <zecarlos@MacBook-Pro-de-Jose-8.local>
2016-03-16 16:50:21 +0000

    Alternativas a EXTRAS

---
+++ Projecto/Exercicio 1/extra.pl
@@ -0,0 +1,11 @@
+% EXTRA
+
+% utentes_profissional: Profissional, Lista de Utentes -> {V, F}
+% utentes_instituicao: Instituicao, Lista de Utentes -> {V, F}
+% utentes_profissional_Instituicao: Instituicao, Profissional, Lista de Utentes -> {V, F}
+% profissionais_servico_instituicao: Instituicao, Servico, Lista de Profissionais -> {V, F}
+
+
+%% Se quiserem acrescentar faixa etária no utente
+%% % utente: Nome, Idade, Serviço, Profissional, Instituição -> {V, F}
+%% Pode-se calcular tipo médias de idades, ...
