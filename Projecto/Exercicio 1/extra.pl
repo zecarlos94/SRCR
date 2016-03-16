42b72a579ae90c82646c21ae6c70f5bbeedf586e
zecarlos94 <zecarlos_94@hotmail.com>
2016-03-16 21:50:54 +0000

    extras
    Adicionei ficheiro novo com extras (sem realizar testes)

--- Projecto/Exercicio 1/extra.pl
+++ Projecto/Exercicio 1/extra.pl
@@ -1,13 +1,7 @@
-% EXTRA
+﻿% EXTRA
 
 % utentes_profissional: Profissional, Lista de Utentes -> {V, F}
 % utentes_instituicao: Instituicao, Lista de Utentes -> {V, F}
 % utentes_profissional_Instituicao: Instituicao, Profissional, Lista de Utentes -> {V, F}
 % profissionais_servico_instituicao: Instituicao, Servico, Lista de Profissionais -> {V, F}
 % utentes_ordenados_alfabeticamente: Lista de Utentes, Lista de Utentes -> {V, F}
-
-
-
-%% Se quiserem acrescentar faixa etária no utente
-%% % utente: Nome, Idade, Serviço, Profissional, Instituição -> {V, F}
-%% Pode-se calcular tipo médias de idades, ordenar por idades.
