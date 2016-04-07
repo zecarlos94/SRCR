f248f8daf7c79d62957174c413dc650db76c669e
zecarlos94 <zecarlos@MacBook-Pro-de-Jose-8.local>
2016-04-07 12:44:50 +0100

    Inicio Exercício 2. Introdução java swing funcional, sem jasper

---
+++ Projecto/Exercício 2/SRCR_TP2/src/srcr_tp2/SRCR_TP2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srcr_tp2;
+
+/**
+ *
+ * @author zecarlos
+ */
+public class SRCR_TP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interface i = new Interface();
+        i.setVisible(true);
+    }
+    
+}
