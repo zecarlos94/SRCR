8ec47131aaf7c1141aedbd781fcf35f9abf61f61
zecarlos94 <zecarlos@MBP-de-Jose-8.home>
2016-04-11 20:55:16 +0100

    interface 2

--- Projecto/Exercício 2/Prolog/bin/SRCR_TP2/src/srcr_tp2/SRCR_TP2.java
+++ Projecto/Exercício 2/Prolog/bin/SRCR_TP2/src/srcr_tp2/SRCR_TP2.java
@@ -16,7 +16,8 @@ public class SRCR_TP2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Interface i = new Interface();
+        //Interface i = new Interface();
+        InterfaceFinal i = new InterfaceFinal();
         i.setVisible(true);
     }
     
